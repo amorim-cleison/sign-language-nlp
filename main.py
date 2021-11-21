Tentative fix for score calculation

main.py
@@ -58,7 +58,7 @@ def run(args):
         X = torch.stack(X)
         X_lengths = torch.stack(X_lengths)
         y = torch.stack(y)
-        return {"X": X, "lengths": X_lengths, "y": y}, y
+        return {"X": X, "lengths": X_lengths, "y": y}, y.cpu()
 
     net_params["iterator_train__collate_fn"] = collate
     net_params["iterator_valid__collate_fn"] = collate
@@ -97,10 +97,10 @@ def run_training_cv(net, dataset, cross_validator, scoring, n_jobs):
     test, train = dataset.collated().split(0.2, indices_only=False)
 
     # Fit:
-    net.fit(train, train.y().cpu())
+    net.fit(train, train.y())
 
     # Score:
-    score = net.score(test, test.y().cpu())
+    score = net.score(test, test.y())
     log(f"Test score: {score:.4f}")
 
     # Cross-validation: