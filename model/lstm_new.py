055b82dd40e9d839d1c31fc62fc89b1017d946a3
Cleison Amorim <amorim.cleison@gmail.com>
2022-04-22 23:21:42 -0300

    Adding padding pack/unpack to LSTM

--- model/lstm_new.py
+++ model/lstm_new.py
@@ -18,13 +18,13 @@ class LSTMNew(nn.Module):
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.batch_first = batch_first
+        self.src_vocab = src_vocab
+        self.tgt_vocab = tgt_vocab
 
-        self.src_ntoken = len(src_vocab)
-        self.tgt_ntoken = len(tgt_vocab)
         src_pad_idx = util.get_pad_idx(src_vocab)
 
         # Layers
-        self.embedding = nn.Embedding(num_embeddings=self.src_ntoken,
+        self.embedding = nn.Embedding(num_embeddings=len(src_vocab),
                                       embedding_dim=input_size,
                                       padding_idx=src_pad_idx)
         self.lstm = nn.LSTM(input_size=input_size,
@@ -33,7 +33,7 @@ class LSTMNew(nn.Module):
                             batch_first=batch_first)
         self.drop = nn.Dropout(dropout)
         self.linear = nn.Linear(in_features=hidden_size,
-                                out_features=self.tgt_ntoken)
+                                out_features=len(tgt_vocab))
         self.softmax = nn.LogSoftmax(dim=-1)
 
         # Weights
@@ -48,17 +48,19 @@ class LSTMNew(nn.Module):
         self.device = device
         return super().to(device)
 
-    def forward(self, X, **kwargs):
+    def forward(self, X, lengths, **kwargs):
         hidden = self.init_hidden(self.get_batch_size(X))
 
         embedded = self.embedding(X)
         output = self.drop(embedded)
 
-        # FIXME: should apply pack/unpack padded sequence?
+        output = self.pack(output, X, lengths)  # Pack
 
         output, hidden = self.lstm(output, hidden)
-        output = self.get_last_time_step(output)
 
+        output = self.unpack(output)  # Unpack
+
+        output = self.get_last_time_step(output)
         output = self.linear(output)
         output = self.softmax(output)
 
@@ -83,3 +85,24 @@ class LSTMNew(nn.Module):
 
     def get_last_time_step(self, output):
         return output[:, -1, :] if self.batch_first else output[-1, :, :]
+
+    def pack(self, output, X=None, lengths=None):
+        import torch.nn.utils.rnn as t
+
+        assert (X is not None) or (
+            output is not None), "One of `X` or `output` should be informed."
+
+        if lengths is None:
+            lengths = util.resolve_lengths(X, self.src_vocab)
+
+        return t.pack_padded_sequence(input=output,
+                                      lengths=lengths.cpu(),
+                                      batch_first=self.batch_first,
+                                      enforce_sorted=False)
+
+    def unpack(self, output):
+        import torch.nn.utils.rnn as t
+
+        unpacked, _ = t.pad_packed_sequence(sequence=output,
+                                            batch_first=self.batch_first)
+        return unpacked
