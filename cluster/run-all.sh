cb0e74f9b73eeb06d1c15868f158c242fe231d8a
Cleison Amorim <cca5@cin.ufpe.br>
2022-09-17 10:47:02 -0300

    Updating cluster configs

--- cluster/run-all.sh
+++ cluster/run-all.sh
@@ -6,8 +6,8 @@ rm -f /out/*.out
 printf 'Deleting cached datasets...\n'
 rm -f /tmp/*.dataset.tmp
 
-printf 'Submitting all tasks...\n'
-(cd out/ && for FILE in ../tasks/*.slurm; do printf ' %s -> ' "$FILE"; sbatch -p long_gpu $FILE; sleep 10s; done;)
+printf 'Submitting tasks (with interval)...\n'
+(cd out/ && for FILE in ../tasks/*.slurm; do printf ' %s -> ' "$FILE"; sbatch -p long_gpu $FILE; sleep 5s; done;)
 
 printf '\n'
 ./status-cluster.sh
