f87c4c312e960e343561b7ccc4d4807db4005e3d
Cleison Amorim <cca5@cin.ufpe.br>
2022-09-17 00:27:53 -0300

    Adjusting cluster configs

--- cluster/run-all.sh
+++ cluster/run-all.sh
@@ -1,7 +1,10 @@
 mkdir -p out/
 
+printf 'Deleting cached datasets...\n'
+rm -f /tmp/*.dataset.tmp
+
 printf 'Submitting all tasks...\n'
-(cd out/ && for FILE in ../tasks/*.slurm; do printf ' Submitting %s... ' "$FILE"; sbatch -p long_gpu $FILE; sleep 15s; done)
+(cd out/ && for FILE in ../tasks/*.slurm; do printf ' %s -> ' "$FILE"; sbatch -p long_gpu $FILE; sleep 10s; done;)
 
 printf '\n'
 squeue
