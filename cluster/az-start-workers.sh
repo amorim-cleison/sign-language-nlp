7de26fee620c301735ee78b21950474734a6d961
Cleison Amorim <amorim.cleison@gmail.com>
2022-10-16 16:43:45 +0000

    Fixing dask workers naming

--- cluster/az-start-workers.sh
+++ cluster/az-start-workers.sh
@@ -2,6 +2,7 @@ DASK_HOST="20.165.54.123"
 DASK_PORT=5696
 DASK_SOURCE="dist/source.zip"
 THREADS=3
+IP=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 export DASK_DISTRIBUTED__DIAGNOSTICS__NVML=False
 
 
@@ -15,6 +16,6 @@ echo "Initializing workers..."
 IFS=',' read -ra GPU <<< "${CUDA_VISIBLE_DEVICES}"
 for i in "${GPU[@]}"; do
   echo " - Worker for GPU ${i}... (in background)"
-  CUDA_VISIBLE_DEVICES=$i poetry run dask-worker "${DASK_HOST}:${DASK_PORT}" --name "worker-gpu${i}" --nworkers 1 --nthreads ${THREADS} &> ~/repos/sign-language-nlp/cluster/out/az-worker-gpu${i}.out &
+  CUDA_VISIBLE_DEVICES=$i poetry run dask-worker "${DASK_HOST}:${DASK_PORT}" --name "worker-${IP}-gpu${i}" --nworkers 1 --nthreads ${THREADS} &> ~/repos/sign-language-nlp/cluster/out/az-worker-gpu${i}.out &
   # CUDA_VISIBLE_DEVICES=$i DASK_DISTRIBUTED__DIAGNOSTICS__NVML=False singularity exec --nv ~/containers/openpose.sif poetry run dask-cuda-worker "${DASK_HOST}:${DASK_PORT}" &
 done
